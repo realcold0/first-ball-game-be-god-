97ff11fae912d9a2ae77b7a33c67c62b6aec5728
kimyeong-in <72372010+kimyeong-in@users.noreply.github.com>
2020-10-06 17:24:06 +0900

    수정필요(터치이벤트)

---
+++ Assets/script/jump.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class jump : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    bool cheakgage;
+    bool cheaktouch;
+    private double gage;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        cheaktouch = true;
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("aa");
+        cheaktouch = false;
+    }
+
+    void Start()
+    {
+        cheakgage = true;
+    }
+
+    void Update()
+    {
+        if (cheaktouch == true)
+        {
+            if (cheakgage == true)
+            {
+                gage += 1;
+                if (gage >= 100) cheakgage = false;
+            }
+            else if (cheakgage == false)
+            {
+                gage -= 1;
+                if (gage <= 0)
+                {
+                    cheakgage = true;
+                }
+            }
+        }
+        Debug.Log("chaek:" + cheakgage + ", gage:" + gage);
+    }
+}
