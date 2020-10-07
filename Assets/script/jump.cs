3bba1d1be5ce3f6f9959495e39b0c757e6e33bb9
realcold0 <65608503+realcold0@users.noreply.github.com>
2020-10-07 16:35:12 +0900

    Update jump.cs
    2단 점프 구현

--- Assets/script/jump.cs
+++ Assets/script/jump.cs
@@ -14,21 +14,38 @@ public class jump : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public GameObject sphere; //플레이어
     public Slider powerBar; // 파워 바
     public float maxPower = 100; // 파워 최대치
-    
+    private float countJump;
+    public Rigidbody rb;
     
 
 
     public void OnPointerDown(PointerEventData eventData)   //터치를 누르고 있을때 
     {
         cheaktouch = true;
+        if(rb.velocity == new Vector3(0,0,0))     //연속 점프 방지용 눌렀을때 속도가 0이어야 점프카운트  0
+        {
+            countJump = 0;
+        }
     }
 
 
     public void OnPointerUp(PointerEventData eventData)     //터치를 땔때
     {
+
         Debug.Log("aa");
         cheaktouch = false;
-        sphere.GetComponent<Rigidbody>().AddForce(new Vector3(gage*jumpPower,gage*jumpPower,0));    //점프시킴
+        if(countJump == 0 && rb.velocity == new Vector3(0,0,0))
+        {
+            sphere.GetComponent<Rigidbody>().AddForce(new Vector3(gage * jumpPower, gage * jumpPower, 0));    //점프시킴
+            Debug.Log("1단 점프!");
+            countJump = 1;
+        }
+        else if(countJump == 1 )
+        {
+            sphere.GetComponent<Rigidbody>().AddForce(new Vector3(0, gage * jumpPower * 2, 0));    //2단점프시킴
+            Debug.Log("2단 점프!");
+            countJump = 2;
+        }
         gage = 0;   //게이트 초기화
     }
 
@@ -37,6 +54,8 @@ public class jump : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         cheakgage = true;
         cheaktouch = false;
         gage = 0;
+        countJump = 0;
+        rb = sphere.GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -58,7 +77,7 @@ public class jump : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
                 cheakgage = true;
             }
         }
-        Debug.Log("chaek:" + cheakgage + ", gage:" + gage);
+        //Debug.Log("chaek:" + cheakgage + ", gage:" + gage);
     }
 
     
