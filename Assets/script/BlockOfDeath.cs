91658b84f59848e30a0104fd5d4f36d8c6e346ea
realcold0 <65608503+realcold0@users.noreply.github.com>
2020-10-09 16:10:47 +0900

    특수 블럭 2개 스테이지 4개 생성

---
+++ Assets/script/BlockOfDeath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BlockOfDeath : MonoBehaviour
+{
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            SceneManager.LoadScene("SampleScene");
+
+        }
+    }
+}
