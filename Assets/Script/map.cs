e30fe7a4baec4df678656568bce1c1c7c6629224
realcold0 <65608503+realcold0@users.noreply.github.com>
2020-10-10 21:20:02 +0900

    스테이지 생성버그 수정 (실패)

--- Assets/Script/map.cs
+++ Assets/Script/map.cs
@@ -12,19 +12,18 @@ public class map : MonoBehaviour
     public int keepmap; //유지할 맵 수
     public List<GameObject> stagelist = new List<GameObject>();
     private int nextStageTip;
+    private int nextStageTip2;
 
-    // Start is called before the first frame update
     void Start()
     {
         mapcount = 0;
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         int charpositon = (int)(player.position.x / maplength); //캐릭터 스테이지 확인
-        nextStageTip = Random.Range(0, stage.Length);
+        
         if (charpositon + keepmap > mapcount)
         {
             UpdateStage(charpositon + keepmap);
@@ -38,18 +37,19 @@ public class map : MonoBehaviour
         GameObject stageObject = (GameObject)Instantiate(
             stage[nextStageTip],
             new Vector3(tipIndex * maplength, 0,0 ),
-            Quaternion.identity
-        );
+            Quaternion.identity);
+        Debug.Log(nextStageTip + "stage 생성");
         return stageObject;
     }
 
 
-    void UpdateStage(int stage)
+    void UpdateStage(int step)
     {
-        if (stage <= mapcount + 1) return;
+        if (step <= mapcount + 1) return;
         // 지정한 스테이지 팁까지를 작성 
-        for (int i = mapcount + 1; i <= stage; i++)
+        for (int i = mapcount + 1; i <= step; i++)
         {
+            nextStageTip = Random.Range(0, stage.Length);
             GameObject stageObject = CreateStage(i);
 
             // 생성한 스테이지를 관리 리스트에 추가
@@ -57,7 +57,7 @@ public class map : MonoBehaviour
         }
         // 스테이지 보관 상한이 될 때까지 예전 스테이지를 삭제
         while (stagelist.Count > keepmap +1) Destroystage();
-        mapcount = stage;
+        mapcount = step;
 
     }
 
