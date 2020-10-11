1e38757e14a86d902f7b6cc1264a95e94af969d3
kimyeong-in <72372010+kimyeong-in@users.noreply.github.com>
2020-10-11 21:55:16 +0900

    점프블럭스테이지추가

---
+++ Assets/Script/Block/Blockofjump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blockofjump : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("chak");
+            player.GetComponent<Rigidbody>().AddForce(new Vector3(50,300, 0));
+        }
+    }
+}
