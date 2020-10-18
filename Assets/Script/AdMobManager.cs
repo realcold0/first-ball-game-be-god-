b243c536a87fb86153819f413b8fbd6b60d38e17
realcold0 <65608503+realcold0@users.noreply.github.com>
2020-10-18 23:33:36 +0900

    드디어 광고 넣기 성공

---
+++ Assets/Script/AdMobManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleMobileAds.Api;
+
+public class AdMobManager : MonoBehaviour
+{
+    public bool isTestMode;
+    public Text LogText;
+    public Button FrontAdsBtn, RewardAdsBtn;
+
+
+    void Start()
+    {
+        LoadBannerAd();
+    }
+
+
+
+    AdRequest GetAdRequest()
+    {
+        return new AdRequest.Builder().AddTestDevice("FFFE8E42ADE98A91").Build();
+    }
+
+
+
+    #region 배너 광고
+    const string bannerTestID = "ca-app-pub-3940256099942544/6300978111";
+    const string bannerID = "ca-app-pub-2730305901082234/2258898494";
+    BannerView bannerAd;
+
+
+    void LoadBannerAd()
+    {
+        bannerAd = new BannerView(isTestMode ? bannerTestID : bannerID,
+            AdSize.SmartBanner, AdPosition.Bottom);
+        bannerAd.LoadAd(GetAdRequest());
+        ToggleBannerAd(false);
+    }
+
+    public void ToggleBannerAd(bool b)
+    {
+        if (b) bannerAd.Show();
+        else bannerAd.Hide();
+    }
+}
+    #endregion
