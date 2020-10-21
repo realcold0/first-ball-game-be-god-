bde7e88d346f6dd62fc66cb54c73f60b57db52e1
realcold0 <65608503+realcold0@users.noreply.github.com>
2020-10-21 17:09:35 +0900

    출시 준비

---
+++ Assets/Script/LogOut.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogOut : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public void Quit()
+     {
+         #if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+         #elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+         #else
+         Application.Quit();
+         #endif
+     }
+}
